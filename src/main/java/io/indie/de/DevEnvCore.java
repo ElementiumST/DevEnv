ca995d4e14defddac75648422614c95821fc84cd
Igor.Starkov <Igor.Starkov@avalab.io>
2022-01-03 06:53:54 +0500

    Initial commit

---
+++ src/main/java/io/indie/de/DevEnvCore.java
@@ -0,0 +1,17 @@
+package io.indie.de;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DevEnvCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
